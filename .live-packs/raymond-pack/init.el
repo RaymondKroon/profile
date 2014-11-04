0aee148a1cc30a13722f7446ca3feee0ebec13df
Raymond Kroon <raymond@k3n.nl>
2014-11-04 20:34:55 +0100

    shift-tab emacs support

--- .live-packs/raymond-pack/init.el
+++ .live-packs/raymond-pack/init.el
@@ -6,3 +6,16 @@
 ;; Load bindings config
 (live-load-config-file "bindings.el")
 (require 'iso-transl)
+
+(global-set-key (kbd "<backtab>") 'un-indent-by-removing-4-spaces)
+(defun un-indent-by-removing-4-spaces ()
+  "remove 4 spaces from beginning of of line"
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (beginning-of-line)
+      ;; get rid of tabs at beginning of line
+      (when (looking-at "^\\s-+")
+        (untabify (match-beginning 0) (match-end 0)))
+      (when (looking-at "^    ")
+        (replace-match "")))))
