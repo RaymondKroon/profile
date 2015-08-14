5a38e24870527fce248f72a24246d0a2c1696c34
Raymond Kroon <raymond@k3n.nl>
2015-08-14 12:55:39 +0200

    less extra profiles.clj stuff

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,32 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.1"]
-                  [refactor-nrepl "0.2.2"]
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl "1.1.0"]
                   ]
         :dependencies [[raymond/debug-tools "0.1.4"]
-                       [spyscope "0.1.5"]
-                       [leiningen #=(leiningen.core.main/leiningen-version)]
-                       [org.clojure/tools.namespace "0.2.8"]
-                       [io.aviso/pretty "0.1.14"]
-                       [im.chit/vinyasa "0.3.4"]]
+                       [org.clojure/tools.nrepl "0.2.7"]]
         :signing {:gpg-key "raymond@clojars"}
-        :injections
-   [(require 'spyscope.core)
-    (require '[vinyasa.inject :as inject])
-    (require 'io.aviso.repl)
-    (inject/in ;; the default injected namespace is `.`
-
-     [debug-tools.core :refer [break]]
-
-     ;; note that `:refer, :all and :exclude can be used
-     [vinyasa.inject :refer [inject [in inject-in]]]
-
-     ;; imports all functions in vinyasa.pull
-     [vinyasa.pull :all]
-
-     ;; inject into clojure.core
-     clojure.core
-     [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
-
-     ;; inject into clojure.core with prefix
-     clojure.core >
-     [clojure.pprint pprint]
-     [clojure.java.shell sh])]}}
+        }}
