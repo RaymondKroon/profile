e24fd489159bf2bf7484ff35587fdbfdc890ba0d
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-02-22 12:41:35 +0100

    emacs unix encoding

--- .live-packs/raymond-pack/init.el
+++ .live-packs/raymond-pack/init.el
@@ -45,4 +45,9 @@
 
 (custom-set-variables
  '(speedbar-show-unknown-files t)
-)
+ )
+
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(setq-default default-buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
